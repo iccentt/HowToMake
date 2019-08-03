5f68696bc5e8c683e6d655a3c4efa15dbbb0d28e
NguyenThanhTung <icce@pop-os.localdomain>
2019-08-03 11:30:43 +0900

    Add Player Movement

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -5,16 +5,20 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody rb;
-    public float moveFoward = 500f;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+    public float moveFoward = 2000f;
+    public float moveSide = 500f;
     // Update is called once per frame
     void FixedUpdate()
     {
         rb.AddForce(0, 0, moveFoward * Time.deltaTime);
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(moveSide*Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-moveSide*Time.deltaTime, 0, 0);
+        }
     }
 }
